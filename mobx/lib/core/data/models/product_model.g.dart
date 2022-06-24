1a9e976a26849b2902479e24c5a0253eb5b27dd7
joshuadeguzman <joshua@joshuamdeguzman.com>
2022-06-24 16:04:15 +0800

    Add example usage of json_annotation

---
+++ mobx/lib/core/data/models/product_model.g.dart
@@ -0,0 +1,21 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'product_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$_ProductModel _$$_ProductModelFromJson(Map<String, dynamic> json) =>
+    _$_ProductModel(
+      id: json['id'] as String,
+      name: json['name'] as String,
+      description: json['description'] as String?,
+    );
+
+Map<String, dynamic> _$$_ProductModelToJson(_$_ProductModel instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'name': instance.name,
+      'description': instance.description,
+    };
--- mobx/lib/products/widgets/product_item.dart
+++ mobx/lib/products/widgets/product_item.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class ProductItem extends StatelessWidget {
+  const ProductItem({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Container();
